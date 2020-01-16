4a1437f968578970d0dd4862c1434f19f59ce415
Shazyy <yarukemc@gmail.com>
2020-01-16 15:55:22 +0100

    add direct url for embed

--- assets/js/main.js
+++ assets/js/main.js
@@ -229,6 +229,7 @@ window.onload = () => {
 	
 	document.querySelector('.share-link').value= window.location.href;
 	document.querySelector('.embed-link').value = '<iframe height="180px" width="500px" frameborder="0" src="https://livecounts.io/yt-sub-counter/embed/?c='+user+'" allowfullscreen></iframe>';
+	document.querySelector('.embed-obs-link').value = 'https://livecounts.io/yt-sub-counter/embed/?c='+user;
 	$(".links").load("/assets/global/other.html");
 
 	if (getUrlVars["t"] == "0") {
