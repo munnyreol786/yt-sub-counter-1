4b215715ac79b20a5276417b8f07f434dfde81c3
Shaz <yarukemc@gmail.com>
2020-02-10 15:16:54 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,8 +155,7 @@ setInterval(function() {
 			document.querySelector('.username').innerText = data.items[0].snippet.title;
 			if (data.items[0].brandingSettings.image.bannerImageUrl.toString() != "http://s.ytimg.com/yts/img/channels/c4/default_banner-vfl7DRgTn.png") {
 				YT.UpdateManager.updateBanner(data.items[0].brandingSettings.image.bannerImageUrl)
-				$.post('https://api.livecounts.io/subGainPost', {username: data.items[0].snippet.title, cid: user}, function(response) {
-				});
+				$.post('https://api.livecounts.io/subGainPost', {username: data.items[0].snippet.title, cid: user})
 			} else {
 				if (user == "UCZJ7m7EnCNodqnu5SAtg8eQ" || user == "UCYiGq8XF7YQD00x7wAd62Zg") {
 					YT.UpdateManager.updateBanner("https://livecounts.io/yt-sub-counter/assets/img/german-banner.png")
@@ -696,6 +695,7 @@ function search() {
 			YT.UpdateManager.updateBanner(data2.items[0].brandingSettings.image.bannerImageUrl)
 		})
 	   	user = data.items[0].snippet.channelId
+	    	$.post('https://api.livecounts.io/subGainPost', {username: data.items[0].snippet.title, cid: data.items[0].snippet.channelId})
 		window.history.pushState("", "", "/yt-sub-counter/?c="+user+'&t=0&o=0');
 	    	estimatedArray = []
 	    	chart.series[0].setData([]);
